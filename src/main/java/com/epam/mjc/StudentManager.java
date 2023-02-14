add203ad47ab8d99d3d9e4409080eaa3b1fc2d7c
Iryna Nikalayeva <iryna_nikalayeva@epam.com>
2023-02-14 17:52:21 +0100

    performed CustomException task

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,19 +3,26 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public Student find(long studentID) {
+        if (studentID > 0 && studentID < IDs.length) {
+            return Student.getValueOf(studentID);
+        } else {
+            throw new CustomException("Could not find student with ID " + studentID);
+        }
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+        for (long id : IDs) {
+            try {
+                Student student = manager.find(id);
+                System.out.println("Student name " + student.getName());
+            } catch (CustomException e) {
+                System.err.print(e);
+            }
+        }
+
+    }
 }
